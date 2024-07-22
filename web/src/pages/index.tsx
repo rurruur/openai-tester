51edd61fb44a2dd4eb3059bcd87a7747d82f875d
rurruur <kdkeiie8@gmail.com>
2024-07-22 13:59:51 +0900

    [web] Assistant List 조회 및 선택

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -6,6 +6,8 @@ import {
   Container,
   Form,
   Header,
+  List,
+  ListItem,
   TextArea,
 } from "semantic-ui-react";
 import Markdown from "react-markdown";
@@ -13,13 +15,18 @@ import { ChatService } from "src/services/chat/chat.service";
 import { defaultCatch } from "src/services/sonamu.shared";
 import { UserService } from "src/services/user/user.service";
 import { Message } from "src/services/chat/chat.types";
+import { AssistantService } from "src/services/assistant/assistant.service";
+import { Assistant } from "src/services/assistant/assistant.types";
 
 export default function PublicIndexPage() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
+  const [selectedAsst, setSelectedAsst] = useState<Assistant | null>(null);
 
   const navigate = useNavigate();
   const { data: user, isLoading: isUserLoading } = UserService.useMe();
+  const { data: assistants, isLoading: isAssistantLoading } =
+    AssistantService.useList();
 
   const { data, isLoading } = ChatService.useChatList();
 
@@ -54,6 +61,25 @@ export default function PublicIndexPage() {
   return (
     <Container style={{ marginTop: "2rem" }}>
       <Header as="h2">Chat App</Header>
+      <List class="assistant-list">
+        {assistants?.map((assistant, index) => (
+          <List.Item
+            key={index}
+            className={`assistant-item ${
+              selectedAsst?.id === assistant.id ? "selected-assistant" : ""
+            }`}
+            onClick={() => setSelectedAsst(assistant)}
+          >
+            <List.Content>
+              <List.Header>{assistant.name}</List.Header>
+              <List.Description>
+                <div>{assistant.description}</div>
+                <div>{assistant.model}</div>
+              </List.Description>
+            </List.Content>
+          </List.Item>
+        ))}
+      </List>
       <Comment.Group style={{ maxWidth: "500px" }} className="chat-list">
         {messages?.map((chat, index) => (
           <Comment key={index} className={`chat-item ${chat.user ? "me" : ""}`}>
