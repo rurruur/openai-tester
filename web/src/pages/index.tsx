fd6f5fcafa4b6f94da7278bc5f6f3ebb1e791eb1
rurruur <kdkeiie8@gmail.com>
2024-07-22 14:49:33 +0900

    [web] 스레드 리스트 및 채팅 리스트 조회

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -17,28 +17,30 @@ import { UserService } from "src/services/user/user.service";
 import { Message } from "src/services/chat/chat.types";
 import { AssistantService } from "src/services/assistant/assistant.service";
 import { Assistant } from "src/services/assistant/assistant.types";
+import { ThreadService } from "src/services/thread/thread.service";
+import { Thread } from "src/services/thread/thread.types";
 
 export default function PublicIndexPage() {
   const [messages, setMessages] = useState<Message[]>([]);
-  const [inputValue, setInputValue] = useState<string>("");
+  const [content, setContent] = useState<string>("");
   const [selectedAsst, setSelectedAsst] = useState<Assistant | null>(null);
+  const [selectedThread, setSelectedThread] = useState<Thread | null>(null);
 
   const navigate = useNavigate();
   const { data: user, isLoading: isUserLoading } = UserService.useMe();
   const { data: assistants, isLoading: isAssistantLoading } =
     AssistantService.useList();
-
-  const { data, isLoading } = ChatService.useChatList();
+  const { data: threads, isLoading: isThreadLoading } = ThreadService.useList();
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setInputValue(e.target.value);
+    setContent(e.target.value);
   };
 
   const handleFormSubmit = () => {
-    if (inputValue.trim() !== "") {
-      ChatService.chat({ content: inputValue })
+    if (content.trim() !== "") {
+      ChatService.chat({ content })
         .then((res) => {
-          setInputValue("");
+          setContent("");
           setMessages(res);
         })
         .catch(defaultCatch);
@@ -53,10 +55,14 @@ export default function PublicIndexPage() {
   }, [user, isUserLoading]);
 
   useEffect(() => {
-    if (data) {
-      setMessages(data);
+    if (selectedThread) {
+      ChatService.getChatList(selectedThread.id)
+        .then((res) => {
+          setMessages(res);
+        })
+        .catch(defaultCatch);
     }
-  }, [data]);
+  }, [selectedThread]);
 
   return (
     <Container style={{ marginTop: "2rem" }}>
@@ -66,7 +72,7 @@ export default function PublicIndexPage() {
           <List.Item
             key={index}
             className={`assistant-item ${
-              selectedAsst?.id === assistant.id ? "selected-assistant" : ""
+              selectedAsst?.id === assistant.id ? "selected-item" : ""
             }`}
             onClick={() => setSelectedAsst(assistant)}
           >
@@ -80,25 +86,57 @@ export default function PublicIndexPage() {
           </List.Item>
         ))}
       </List>
-      <Comment.Group style={{ maxWidth: "500px" }} className="chat-list">
-        {messages?.map((chat, index) => (
-          <Comment key={index} className={`chat-item ${chat.user ? "me" : ""}`}>
-            <Comment.Content>
-              <Comment.Text>
-                <Markdown>{chat.content}</Markdown>
-              </Comment.Text>
-            </Comment.Content>
-          </Comment>
-        ))}
-      </Comment.Group>
-      <Form reply onSubmit={handleFormSubmit} style={{ marginTop: "1rem" }}>
-        <TextArea
-          value={inputValue}
-          onChange={handleInputChange}
-          placeholder="Type your message..."
-        />
-        <Button content="Send" labelPosition="left" icon="send" primary />
-      </Form>
+      <div className="main">
+        <List className="thread-list">
+          {threads?.map((thread, index) => (
+            <ListItem
+              key={index}
+              className={`thread-item ${
+                selectedThread?.id === thread.id ? "selected-item" : ""
+              }`}
+              onClick={() => setSelectedThread(thread)}
+            >
+              <List.Content>
+                <List.Header>{`채팅 ${index + 1}`}</List.Header>
+                <List.Description>
+                  <div>{thread.created_at}</div>
+                </List.Description>
+              </List.Content>
+            </ListItem>
+          ))}
+        </List>
+        <div className="chat">
+          {selectedThread && (
+            <>
+              <List className="chat-list">
+                {messages?.map((chat, index) => (
+                  <List.Item
+                    key={index}
+                    className={`chat-item ${chat.user ? "me" : ""}`}
+                  >
+                    <List.Content>
+                      <Markdown>{chat.content}</Markdown>
+                    </List.Content>
+                  </List.Item>
+                ))}
+              </List>
+              <Form reply onSubmit={handleFormSubmit}>
+                <TextArea
+                  value={content}
+                  onChange={handleInputChange}
+                  placeholder="Type your message..."
+                />
+                <Button
+                  content="Send"
+                  labelPosition="left"
+                  icon="send"
+                  primary
+                />
+              </Form>
+            </>
+          )}
+        </div>
+      </div>
     </Container>
   );
 }
